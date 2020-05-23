570d5d562a456ad18941605fc07f5cae130e0ca9
Алексей Носов <anosov@seven.tech>
2020-05-23 13:45:01 +0300

    добваил spring 5 new project

---
+++ spring5learn/src/main/java/ru/dvimer/spring5learn/Spring5learnApplication.java
@@ -0,0 +1,13 @@
+package ru.dvimer.spring5learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5learnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5learnApplication.class, args);
+	}
+
+}
---
+++ spring5learn/src/test/java/ru/dvimer/spring5learn/Spring5learnApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dvimer.spring5learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5learnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
