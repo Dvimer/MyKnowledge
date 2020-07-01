665d82c65097ca21f85c165408c70cdc46eaf1b5
Алексей Носов <anosov@seven.tech>
2020-07-01 14:33:23 +0300

    create catalizator gradle

---
+++ catalizator/src/main/java/ru/dvimer/catalizator/router/GreetingRouter.java
@@ -0,0 +1,32 @@
+package ru.dvimer.catalizator.router;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RequestPredicate;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import ru.dvimer.catalizator.handler.GreetingHandler;
+
+@Configuration
+public class GreetingRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> route(GreetingHandler greetingHandler) {
+
+        RequestPredicate route = RequestPredicates.GET("/hello")
+                .and(RequestPredicates.accept(MediaType.TEXT_PLAIN));
+
+        return RouterFunctions
+                .route(route, greetingHandler::hello)
+                .andRoute(RequestPredicates.GET("/"), serverRequest -> {
+                    return ServerResponse
+                            .ok()
+                            .contentType(MediaType.TEXT_PLAIN)
+                            .body(BodyInserters.fromValue("Main, page!"));
+                });
+    }
+}
